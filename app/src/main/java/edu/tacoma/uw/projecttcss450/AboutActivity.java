24ad4e62070cc1f3009137eef62329e0dedaec84
Jungyhon Jo <whwheoeo@uw.edu>
2024-04-18 20:32:54 -0700

    4/18/2024 revision

--- app/src/main/java/edu/tacoma/uw/projecttcss450/AboutActivity.java
+++ app/src/main/java/edu/tacoma/uw/projecttcss450/AboutActivity.java
@@ -8,17 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        String Developer1 = getString(R.string.Developer3_info);
+        String Developer2 = getString(R.string.Developer2_info);
+        String Developer3 = getString(R.string.Developer1_info);
+
     }
 }
