2c26f230a817ea180409fd63b4dfd2924f8a479e
edward <edwardspotgaming@gmail.com>
2024-05-01 09:03:13 -0700

    Refactored to make names neater.

--- app/src/main/java/edu/tacoma/uw/projecttcss450/CampusMapActivity.java
+++ app/src/main/java/edu/tacoma/uw/projecttcss450/CampusMapActivity.java
@@ -11,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CampusMap extends AppCompatActivity {
+public class CampusMapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/edu/tacoma/uw/projecttcss450/ParkingActivity.java
+++ app/src/main/java/edu/tacoma/uw/projecttcss450/ParkingActivity.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class Parking extends AppCompatActivity {
+public class ParkingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +16,11 @@ public class Parking extends AppCompatActivity {
         // Initialize the ListView and set the adapter
         ListView listView = findViewById(R.id.parking_list);
         String[] parkingLocations = {
-                "Court 17 Parking Garage",
-                "Tacoma Dome Station Parking Garage",
-                "Broadway Parking Garage",
-                "Market Street Parking Garage",
-                "Pacific Avenue Parking Garage"
+                "Court 17 ParkingActivity Garage",
+                "Tacoma Dome Station ParkingActivity Garage",
+                "Broadway ParkingActivity Garage",
+                "Market Street ParkingActivity Garage",
+                "Pacific Avenue ParkingActivity Garage"
         };
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, parkingLocations);
         listView.setAdapter(adapter);
