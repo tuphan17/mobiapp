2891fbd1f9f9c5dcb1b045bbb66e871145b66493
Jungyhon Jo <whwheoeo@uw.edu>
2024-05-02 20:36:28 -0700

    Revised ClassPlanActivity

---
+++ app/src/main/java/edu/tacoma/uw/projecttcss450/AddCourse.java
@@ -0,0 +1,22 @@
+package edu.tacoma.uw.projecttcss450;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * create an instance of this fragment.
+ */
+public class AddCourse extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_course, container, false);
+    }
+}
