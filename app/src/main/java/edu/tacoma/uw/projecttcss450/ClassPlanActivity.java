76773aecb30a3e2261da0e60bea6442f0f5034aa
Jungyhon Jo <whwheoeo@uw.edu>
2024-05-02 20:36:28 -0700

    Revised ClassPlanActivity

--- app/src/main/java/edu/tacoma/uw/projecttcss450/ClassPlanActivity.java
+++ app/src/main/java/edu/tacoma/uw/projecttcss450/ClassPlanActivity.java
@@ -1,6 +1,10 @@
 package edu.tacoma.uw.projecttcss450;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,9 +25,36 @@ public class ClassPlanActivity extends AppCompatActivity {
             return insets;
         });
 
-        String freshMan = getString(R.string.Fresh_button);
-        String transfer = getString(R.string.Transfer_button);
-        String graduation = getString(R.string.Graduation_button);
+        String apply = getString(R.string.To_Apply);
+        String graduate = getString(R.string.To_Graduate);
+        String classPlan = getString(R.string.Class_Plan);
+
+        findViewById(R.id.ApplyButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://students.washington.edu/whwheoeo/toApplyMajor.html");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        findViewById(R.id.GraduationButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://students.washington.edu/whwheoeo/toGraduation.html");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        Button classButton = findViewById(R.id.myPlanButton);
+        classButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClassPlanActivity.this, AddCourseActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
