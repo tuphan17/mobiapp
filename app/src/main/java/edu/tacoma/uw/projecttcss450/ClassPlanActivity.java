71b1128d1f246b20e73f2051592b0d7ab35f2ab1
edwardspot <edwardspotgaming@gmail.com>
2024-05-17 19:18:53 -0700

    fix

--- app/src/main/java/edu/tacoma/uw/projecttcss450/ClassPlanActivity.java
+++ app/src/main/java/edu/tacoma/uw/projecttcss450/ClassPlanActivity.java
@@ -79,28 +79,6 @@ public class ClassPlanActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //Bottom Navigation Bar
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            int itemId = item.getItemId();
-            if (itemId == R.id.bottomNavHome) {
-                startActivity(new Intent(this, MainActivity.class));
-                return true;
-            } else if (itemId == R.id.bottomNavClassPlan) {
-                startActivity(new Intent(this, ClassPlanActivity.class));
-                return true;
-            } else if (itemId == R.id.bottomNavCampusMap) {
-                startActivity(new Intent(this, CampusMapActivity.class));
-                return true;
-            } else if (itemId == R.id.bottomNavParking) {
-                startActivity(new Intent(this, ParkingActivity.class));
-                return true;
-
-                // Add more cases for additional menu items
-            }
-            return false;
-        });
-
 
 
     }
